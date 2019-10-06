2facd562d58b42d860767011443bae7e6b47a709
kasmide <kasmide@zohomail.com>
2019-10-07 08:07:34 +0900

    improve server reliability

--- p.php
+++ p.php
@@ -1,10 +1,10 @@
 <?php
-if ($_GET['id'] != NULL) {
+if (isset($_GET['id'])) {
     $content = file_get_contents("https://ext.nicovideo.jp/api/getthumbinfo/${_GET['id']}");
-} elseif ($_GET['q'] != NULL) {
+} elseif (isset($_GET['q'])) {
     $query = urlencode($_GET['q']);
     $content = file_get_contents("https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=${query}&targets=title&fields=thumbnailUrl,contentId,title,viewCounter,description&_sort=-mylistCounter&_offset=0&_limit=50");
-} elseif ($_GET['related'] != NULL) {
+} elseif (isset($_GET['related'])) {
     $content = file_get_contents("http://flapi.nicovideo.jp/api/getrelation?video=${_GET['related']}");
 }
 if($content == ""){$content="{\"ERROR\":\"NO_CONTENT_RECEIVED\"}";}
