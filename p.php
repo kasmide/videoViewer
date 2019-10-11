8f502e333d1d5f4439b7e538608f2ef977a2c23c
kasmide <kasmide@zohomail.com>
2019-10-11 21:39:04 +0900

    Add support for changing targets to search for

--- p.php
+++ p.php
@@ -1,9 +1,15 @@
 <?php
+if(isset($_GET['target'])){
+    $targets = $_GET['q'];
+}else{
+    $targets = "title,description,tags";
+}
+
 if (isset($_GET['id'])) {
     $content = file_get_contents("https://ext.nicovideo.jp/api/getthumbinfo/${_GET['id']}");
 } elseif (isset($_GET['q'])) {
     $query = urlencode($_GET['q']);
-    $content = file_get_contents("https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=${query}&targets=title&fields=thumbnailUrl,contentId,title,viewCounter,description&_sort=-mylistCounter&_offset=0&_limit=50");
+    $content = file_get_contents("https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=${query}&targets=${targets}&fields=thumbnailUrl,contentId,title,viewCounter,description&_sort=-mylistCounter&_offset=0&_limit=50");
 } elseif (isset($_GET['related'])) {
     $content = file_get_contents("http://flapi.nicovideo.jp/api/getrelation?video=${_GET['related']}");
 }
